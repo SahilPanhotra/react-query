15f1c45cab199c5cf17ec97a78b824f7e9c21327
SahilPanhotra <sahilpanhotra@gmail.com>
2023-11-07 11:48:36 +0530

    configure react query and load issues list with avatar

---
+++ src/helpers/useUserData.js
@@ -0,0 +1,11 @@
+import { useQuery } from "react-query";
+
+export function useUserData(userId) {
+  const userData = useQuery(
+    ["user", userId],
+    () => fetch(`/api/users/${userId}`).then((res) => res.json()),
+    { enabled: !!userId }
+  );
+
+  return userData;
+}
